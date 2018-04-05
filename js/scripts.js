6f2ef8dec11bf92483224ce7b19179ce60b2f81c
Brea Borlas and Garnett Puett <brea@>
2018-04-05 14:00:13 -0700

    Add cpu play

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 function Player (mark) {
   this.mark = mark;
+  this.cpu = false;
 }
 
 playerX = new Player("X");
 playerO = new Player("O");
 
 var currentPlayer = playerX;
-
-function switchPlayer() {
-  if (currentPlayer === playerX){
-    currentPlayer = playerO;
-  } else {
-    currentPlayer = playerX;
-  }
-}
 
 function Space(position) {
   this.position = position;
@@ -31,6 +24,36 @@ space7 = new Space(7);
 space8 = new Space(8);
 
 var board = [space0, space1, space2, space3, space4, space5, space6, space7, space8];
+
+
+function freeSpotFinder(){
+  var freeSpot;
+  board.forEach(function(spot) {
+    if(spot.mark === 0) {
+      freeSpot = spot;
+    }
+});
+  return freeSpot;
+}
+
+function cpuPlay() {
+  var spot = freeSpotFinder();
+  game(spot.position);
+  makeMark($("#spot-" + spot.position));
+  currentPlayer = playerX;
+  $("#player").text("Player " + currentPlayer.mark);
+}
+
+function switchPlayer() {
+  if (currentPlayer === playerX){
+    currentPlayer = playerO;
+    if(playerO.cpu === true) {
+      cpuPlay();
+    }
+  } else {
+    currentPlayer = playerX;
+  }
+}
 
 function isAWin() {
   if (board[0].mark === board[1].mark && board[1].mark === board[2].mark && board[2].mark !== 0) {
@@ -67,27 +90,36 @@ function isADraw() {
   return false;
 }
 
+
+
 function game(spot) {
   board[spot].mark = currentPlayer.mark;
+  //debugger;
   if (isAWin()) {
     alert(currentPlayer.mark + " wins!");
+    location.reload();
   } else if (isADraw()) {
     alert("Draw!");
     location.reload();
   }
 }
 
-
+//user interfaces
 function makeMark(spot) {
   $(spot).text(currentPlayer.mark);
 }
 
 $(document).ready(function(){
-  $(".btn").click(function(event) {
+  $(".btn-light").click(function(event) {
     event.preventDefault();
     game($(this).val());
     makeMark($(this).parents(".spot"));
     switchPlayer();
     $("#player").text("Player " + currentPlayer.mark);
   });
+
+  $("#cpu").click(function(event) {
+    event.preventDefault();
+    playerO.cpu = true;
+  })
 });
